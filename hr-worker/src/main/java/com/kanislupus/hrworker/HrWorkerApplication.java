c7bc11e123eac0371a16b7e774c12070f3620a79
PedroHBCruz <75803558+PedroHBCruz@users.noreply.github.com>
2022-01-25 13:59:54 -0400

    Project hr-worker created

---
+++ hr-worker/src/main/java/com/kanislupus/hrworker/HrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.kanislupus.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+}
